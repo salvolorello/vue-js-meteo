262e96b7e2303c03529d2812ecb1bd4c9f65885c
Salvatore Lorello <salvatore.lorello@liparipeople.com>
2023-05-23 14:31:40 +0200

    path correct

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: '/vue-js-meteo/'
 })
